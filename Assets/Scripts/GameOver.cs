c1bcc23170f8ea09924f71b1e09f32f8f0d84e1e
Gemma <gemma.torello@hotmail.com>
2025-01-15 13:53:56 +0100

    REFACTOR: Removed unused game objects

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -6,34 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    [SerializeField] GameObject GameOverPanel;
-    [SerializeField] TMP_Text TimeText;
-    [SerializeField] TMP_Text ScoreText;
-    [SerializeField] GameObject retryButton;
+    [SerializeField] TMP_Text timeText;
+    [SerializeField] TMP_Text scoreText;
+
+    GameObject retryButton;
     
-    [SerializeField] PlayerDataController playerDataController;
+    //[SerializeField] PlayerDataController playerDataController;
 
     public void UpdateView(ResultOfTheRound result)
     {
         var roundDuration = (float)result.Duration.TotalSeconds;
-        ScoreText.text = result.HitsProportion.ToString();
-        TimeText.text = TimeFormat.AsStopwatch(roundDuration);
+        scoreText.text = result.HitsProportion.ToString();
+        timeText.text = TimeFormat.AsStopwatch(roundDuration);
 
         if(result.HasLost)
             Lose();
     }
-    
+
+    private void ChangeScene( int index )
+    {
+        //playerDataController.SendMessage( "SaveToJson" );
+        SceneManager.LoadScene( index );
+    }
+
     public void Retry()
     {
         ChangeScene( SceneManager.GetActiveScene().buildIndex );
     }
-    
-    private void ChangeScene( int index )
-    {
-        playerDataController.SendMessage( "SaveToJson" );
-        SceneManager.LoadScene( index );
-    }
-    
+
     public void GoToMainMenu()
     {
         int mainMenuIndexScene = 1;
@@ -42,7 +42,8 @@ public class GameOver : MonoBehaviour
 
     public void Lose()
     {
-        retryButton = GameOverPanel.transform.Find("Retry").gameObject;
+        GameObject gameOverPanel = GameObject.Find( "GameOverPanel" );
+        retryButton = gameOverPanel.transform.Find( "Retry" ).gameObject;
         Assert.IsNotNull( retryButton );
         retryButton.SetActive( true );
     }
